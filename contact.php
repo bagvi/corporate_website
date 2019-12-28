cd68f4f4587320102ba52ed827453a3a9e5df5b3
bagvi <naked_ass@mail.ru>
2019-12-28 20:06:08 +0300

    Add contact and clear_img

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+include 'modules/header&footer/header.html';
+?>
+	<main>
+		<img src="img/img.jpg" alt="карта как добраться" class="mw-location">
+		<div class="wrapper">
+			<div class="location">
+				<div class="adress">
+					<h3>Приходите к нам:</h2><br>
+						<p>Пензенская область, Кстово<br>ул. Инженерная, д. 56</p><br>
+						<img src="img/img.jpg" alt="наше здание" class="mw-building">
+				</div>
+				<div class="telephone">
+					<h3>Звоните нам:</h2><br>
+						<p>Петров Иван Васильевич<br>
+							(8333) 15-55-36<br><a href="mailto:dobby@mail.info">dobby@mail.info</a><br><br>
+							Петров Иван Васильевич<br>
+							(8333) 15-55-36<br><a href="mailto:dobby@mail.info">dobby@mail.info</a><br><br>
+							Петров Иван Васильевич<br>
+							(8333) 15-55-36<br><a href="mailto:dobby@mail.info">dobby@mail.info</a><br><br>
+							Петров Иван Васильевич<br>
+							(8333) 15-55-36<br><a href="mailto:dobby@mail.info">dobby@mail.info</a><br><br>
+						</p>
+					<h2>Всегда Вам рады!</h2>
+				</div>
+			</div>
+		</div>
+	</main>
+<?php
+include 'modules/header&footer/footer.html';
+?>
