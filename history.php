a25f95f9d0e25d2d14d72e9a9cefdd7aa323eab5
bagvi <naked_ass@mail.ru>
2019-12-28 20:21:11 +0300

    Add pages history and links

---
+++ history.php
@@ -0,0 +1,62 @@
+<?php
+include 'modules/header&footer/header.html';
+?>
+<main class="history">
+	<div class="wrapper">
+		<div class="history-cover">
+			<div class="history-body">
+				<div class="history-creature">
+					<div class="history-title">
+						<h1>Lorem ipsum dolor sit amet consectetur adipisicing.
+							<br>06.04.2009г.</h1>
+					</div>
+					<div class="history-text">
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quis quae et placeat? At
+							nesciunt debitis perspiciatis reprehenderit eos dolor.</p>
+					</div>
+					<div><img src="img/img.png"></div>
+				</div>
+			</div>
+			<div class="history-opened">
+				<div class="history-title">
+					<h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt?<br>15.01.2009г.</h1>
+				</div>
+				<div class="history-text-opened">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis incidunt at suscipit a voluptates
+						laborum commodi atque, natus dicta impedit quis modi unde nihil sit assumenda quo voluptate
+						nesciunt ea.Repudiandae placeat soluta quos officiis iusto maxime praesentium corrupti, itaque
+						atque quis et labore doloribus laboriosam obcaecati accusamus reiciendis tempora autem totam est
+						molestiae repellat deleniti quas quisquam. Explicabo, nulla?</p>
+				</div>
+				<img src="img/img.jpg">
+			</div>
+		</div>
+		<div class="certificate-cover">
+			<div class="history-creature">
+				<div class="history-title">
+					<h1>Lorem, ipsum dolor.<br>02.12.2009г.</h1>
+				</div>
+				<div class="history-text">
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit excepturi doloremque minus nobis,
+						dicta vero necessitatibus aspernatur tempora. Illo reprehenderit impedit officiis voluptas quae
+						earum omnis, expedita esse corporis nobis!</p>
+				</div>
+				<div><img src="img/img.png"></div>
+			</div>
+			<div class="history-opened">
+				<div class="history-title">
+					<h1>Lorem, ipsum.<br>12.01.2011г.</h1>
+				</div>
+				<div class="history-text-opened">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa quod itaque architecto
+						dolorem aut et rem repellendus odit, vel autem, reiciendis, neque ad blanditiis vitae eos alias
+						enim nam.Omnis veniam voluptatem porro atque excepturi error cumque praesentium aspernatur
+						ratione distinctio, at et.</p>
+				</div>
+				<img src="img/img.png">
+			</div>
+		</div>
+	</div>
+<?php
+include 'modules/header&footer/footer.html';
+?>
---
+++ links.php
@@ -0,0 +1,30 @@
+<?php
+include 'modules/header&footer/header.html';
+?>
+<main>
+	<div class="links">
+		<div class="wrapper">
+			<div class="links-text">
+				<section>
+					<h2 class="head_title">Образование</h2>
+					<p><a target="_blank" href="#">Система дистанционного обучения</a></p>
+					<p><a target="_blank" href="#">Электронный журнал</a></p>
+					<p><a target="_blank" href="#">Автоматизированная система контроля</a></p>
+				</section>
+				<section>
+					<h2 class="head_title">Справочники</h2>
+					<p><a target="_blank" href="#">Телефонные справочники компании</a></p>
+				</section>
+				<section>
+					<h2 class="head_title">Интересное</h2>
+					<p><a target="_blank" href="#">КонсультантПлюс</a></p>
+					<p><a target="_blank" href="#">Почтовый клиент</a></p>
+					<p><a target="_blank" href="#">Информационные ресурсы</a></p>
+				</section>
+			</div>
+		</div>
+	</div>
+</main>
+<?php
+include 'modules/header&footer/footer.html';
+?>
