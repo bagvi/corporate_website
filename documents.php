f514d6455d752d68df8d7096e289a8e43c933234
bagvi <naked_ass@mail.ru>
2019-12-31 21:55:54 +0300

    Add folder DOCS with unknown.pdf and documents.php

---
+++ documents.php
@@ -0,0 +1,122 @@
+<?php
+	include 'modules/header&footer/header.html';
+?>
+<body>
+	<main class="wrapper">
+		<h2 class="head_title">Руководящие документы
+		</h2>
+		<div class="docs">
+			<article class="card">
+				<a href="docs/unknown.pdf" class="card__link" target="_blank">
+					<div class="card__header">
+						<h3 class="card__header-title">Lorem ipsum dolor sit amet.</h3>
+						<p class="card__header-meta">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae,
+							quibusdam.</p>
+						<div class="card__header-icon">
+							<svg viewbox="0 0 28 25">
+								<path fill="#109bce"
+									d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</article>
+			<article class="card">
+				<a href="docs/unknown.pdf" class="card__link" target="_blank">
+					<div class="card__header">
+						<h3 class="card__header-title">Lorem ipsum dolor sit amet.</h3>
+						<p class="card__header-meta">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae,
+							quibusdam.</p>
+						<div class="card__header-icon">
+							<svg viewbox="0 0 28 25">
+								<path fill="#109bce"
+									d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</article>
+			<article class="card">
+				<a href="docs/unknown.pdf" class="card__link" target="_blank">
+					<div class="card__header">
+						<h3 class="card__header-title">Lorem ipsum dolor sit amet.</h3>
+						<p class="card__header-meta">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae,
+							quibusdam.</p>
+						<div class="card__header-icon">
+							<svg viewbox="0 0 28 25">
+								<path fill="#109bce"
+									d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</article>
+			<article class="card">
+				<a href="docs/unknown.pdf" class="card__link" target="_blank">
+					<div class="card__header">
+						<h3 class="card__header-title">Lorem ipsum dolor sit amet.</h3>
+						<p class="card__header-meta">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae,
+							quibusdam.</p>
+						<div class="card__header-icon">
+							<svg viewbox="0 0 28 25">
+								<path fill="#109bce"
+									d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</article>
+		</div>
+		<h2 class="head_title">Дополнительные материалы
+		</h2>
+		<div class="docs">
+			<article class="card">
+				<a href="docs/unknown.pdf" class="card__link" target="_blank">
+					<div class="card__header">
+						<h3 class="card__header-title">Lorem ipsum dolor sit amet.</h3>
+						<p class="card__header-meta">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae,
+							quibusdam.</p>
+						<div class="card__header-icon">
+							<svg viewbox="0 0 28 25">
+								<path fill="#109bce"
+									d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</article>
+			<article class="card">
+				<a href="docs/unknown.pdf" class="card__link" target="_blank">
+					<div class="card__header">
+						<h3 class="card__header-title">Lorem ipsum dolor sit amet.</h3>
+						<p class="card__header-meta">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae,
+							quibusdam.</p>
+						<div class="card__header-icon">
+							<svg viewbox="0 0 28 25">
+								<path fill="#109bce"
+									d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</article>
+			<article class="card">
+				<a href="docs/unknown.pdf" class="card__link" target="_blank">
+					<div class="card__header">
+						<h3 class="card__header-title">Lorem ipsum dolor sit amet.</h3>
+						<p class="card__header-meta">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae,
+							quibusdam.</p>
+						<div class="card__header-icon">
+							<svg viewbox="0 0 28 25">
+								<path fill="#109bce"
+									d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</article>
+		</div>
+	</main>
+<?php
+	include 'modules/header&footer/footer.html';
+?>
