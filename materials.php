3fa37d1ac5b89226fd0bfa4abe025ed18c5ecfab
bagvi <naked_ass@mail.ru>
2020-01-01 18:55:14 +0300

    Add materials.php

---
+++ materials.php
@@ -0,0 +1,110 @@
+<?php
+	include 'modules/header&footer/header.html';
+?>
+
+<body>
+	<main>
+		<div class="wrapper">
+			<h2 class="head_title">Информация</h2>
+			<span class="materials-title--span">При нажатии на ссылку будет выполнена автоматическая переадресация на
+				ftp-сервер, навигация осуществляется прямо из браузера, необходимый материал Вы можете сохранить себе на
+				компьютер по клику правой кнопкой мыши и выборе пункта контекстного меню "Сохранить ссылку
+				как...".</span><br>
+			<h2 class="head_title">Видеоматериалы</h2>
+			<section class="video_materials">
+				<div class="general">
+					<a href="ll" target=_blank>Lorem ipsum<br>
+						<span>Lorem ipsum dolor sit amet consectetur</span>
+					</a>
+				</div>
+				<div class="services">
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span style='font-size: 8px'>Lorem ipsum dolor sit amet</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span style='font-size: 8px'>Lorem ipsum dolor sit amet</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span style='font-size: 8px'>Lorem ipsum dolor sit amet</span>
+					</a>
+				</div>
+			</section>
+			<h2 class="head_title">Текстовые материалы</h2>
+
+			<section class="video_materials">
+				<div class="general">
+					<a href="textAll" target=_blank>Lorem, ipsum<br>
+						<span>Lorem ipsum dolor sit amet consectetur</span></a>
+				</div>
+				<div class="services">
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span>Lorem, ipsum dolor</span>
+					</a>
+					<a href="#" target=_blank>A<br>
+						<span style='font-size: 8px'>Lorem ipsum dolor sit amet</span>
+					</a>
+				</div>
+			</section>
+
+			<h2 class="head_title">Онлайн материалы</h2>
+			<section class="video_materials last">
+				<div class="services">
+					<a href="#" target=_blank>Технические средства железных дорог</a>
+					<a href="#" target=_blank>Железнодорожные станции и узлы</a>
+					<a href="#" target=_blank>Размещение и крепление грузов</a>
+					<a href="#" target=_blank>Исскуственные сооружения</a>
+				</div>
+			</section>
+			<span class="materials-title--span" style="text-align: right; font-size:12px; padding-bottom: 48px;">Все
+				размещенные материалы принадлежат правообладателям и могут быть удалены по их требованию.</span>
+		</div>
+	</main>
+	
+<?php
+  include 'modules/header&footer/footer.html';
+?>
