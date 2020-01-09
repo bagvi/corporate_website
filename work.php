e8fa36586109966438ba46965d00b6bc0f31ffb5
bagvi <naked_ass@mail.ru>
2020-01-09 20:58:21 +0300

    Add favicon and add page work.php

---
+++ work.php
@@ -0,0 +1,52 @@
+<?php
+	include 'modules/header&footer/header.html';
+?>
+
+<body>
+	<main>
+
+		<div class="wrapper">
+			<div class="work">
+				<h2 class="head_title">Документы</h2>
+				<div class="download">
+					<a class="download_click" href="#" target="_blank">Первый документ</a>
+					<a class="download_click" href="#" target="_blank">Второй документ</a>
+				</div>
+			</div>
+			<div class="plans">
+				<h2 class="head_title">Планы</h2>
+				<div class="download">
+					<a class="download_click" href="#" target="_blank">Первый план</a>
+					<a class="download_click" href="#" target="_blank">Второй план</a>
+					<a class="download_click" href="#" target="_blank">График</a>
+				</div>
+			</div>
+
+			<div class="other">
+				<h2 class="head_title">Мероприятия</h2>
+				<div class="download">
+					<a class="download_click" href="#" target="_blank">Первое мероприятие</a>
+					<a class="download_click" href="#" target="_blank">Второе мероприятие</a>
+				</div>
+			</div>
+
+			<div class="intresting" style="padding-bottom: 48px;">
+				<h2 class="head_title">Подборка интересных материалов</h2>
+				<div class="download">
+					<a class="download_click" href="#" target="_blank">Дистанционное обучение</a>
+					<a class="download_click" href="#" target="_blank">Реферативные материалы</a>
+					<a class="download_click" href="#" target="_blank">Опыт по обучению</a>
+					<a class="download_click" href="#" target="_blank">Опыт совершенствования</a>
+					<a class="download_click" href="#" target="_blank">Техническая учеба</a>
+					<a class="download_click" href="#" target="_blank">Обучение кадров</a>
+					<a class="download_click" href="#" target="_blank">Организация и проведение</a>
+				</div>
+			</div>
+		</div>
+	</main>
+<?php
+	include 'modules/header&footer/footer.html';
+?>
+</body>
+
+</html>
